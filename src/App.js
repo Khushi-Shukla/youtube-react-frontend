cf510004f632998af84ceb5630d741223fd279d2
unknown <khushishukla0502@gmail.com>
2021-08-04 01:23:58 +0530

    Sidebar component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
-import Header from './components/header/Header';
+import Header from './components/Header/Header';
+import RecomendedVideos from './components/ReccomendedVideos/RecomendedVideos';
+import Sidebar from './components/Sidebar/Sidebar';
 
 
 function App() {
   return (
     <div className="app">
       <Header />
-     
+      <div className="app-page">
+        <Sidebar />
+        <RecomendedVideos />
+      </div>
     </div>
   );
 }
